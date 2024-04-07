c5101d49d08b7c1b6dc821c5fea84692e9aa5976
Riddhi04 <109695389+Riddhigera@users.noreply.github.com>
2024-04-07 23:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (13.59%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkValidString(String s) {
+         int open = 0;
+        for(char ch : s.toCharArray()){
+            if(ch=='(' || ch == '*') open++;
+            else open--;
+
+            if(open < 0) return false;
+        }
+        int close = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch = s.charAt(i);
+            if(ch==')' || ch == '*') close++;
+            else close--;
+
+            if(close < 0) return false;
+        }
+        return true;
+
+        
+    }
+}
