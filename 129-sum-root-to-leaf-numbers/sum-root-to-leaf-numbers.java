3dd0e618fc85e89df39226bcfc03156c3150eef8
Riddhi04 <109695389+Riddhigera@users.noreply.github.com>
2024-04-15 11:43:54 +0530

    Time: 9 ms (7.69%) | Memory: 41.5 MB (17.41%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        List<String> arr = new ArrayList<>();
+        helper(root , arr , "");
+        // System.out.println(arr);
+        int ans = 0;
+        for(String st:arr){
+            ans += Integer.parseInt(st);
+        }
+        return ans;   
+    }
+
+    public void helper(TreeNode root , List<String> arr , String s){
+        if(root==null) return;
+
+        if(root.left==null && root.right==null){
+            s += root.val;
+            arr.add(s);
+            return;
+        }
+        helper(root.left , arr , s + root.val );
+        helper(root.right , arr , s + root.val );
+    }
+}
