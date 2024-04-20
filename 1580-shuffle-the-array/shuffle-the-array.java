eb34fe442aac7b8fd487e0d5c34a5fe7c9adbe15
Riddhi04 <109695389+Riddhigera@users.noreply.github.com>
2024-04-20 19:19:32 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (5.12%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-         n=2*n;
-        int arr1[]=new int[(n/2)];
-        int arr2[]=new int[(n/2)];
-        for(int i=0;i<n/2;i++)
-        arr1[i]=nums[i];
-        for(int j=0,i=(n/2);j<arr2.length;j++,i++)
-        arr2[j]=nums[i];
-        for(int i=0,j=0;i<arr1.length;i++,j+=2)
-        nums[j]=arr1[i];
-        for(int i=0,j=1;i<arr2.length;i++,j+=2)
-        nums[j]=arr2[i];
-        return nums;
+        int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+
+        return result;
+
+        
     }
 }
